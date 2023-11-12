6acd11e84dd67e825bdc1c1f7f53f3650bea6696
MadhuKinnera <kinneramadhu123@gmail.com>
2023-11-12 20:54:57 +0530

    changed upload cloud to get

--- projectk/src/main/java/com/madhu/controller/UtilController.java
+++ projectk/src/main/java/com/madhu/controller/UtilController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,9 +21,11 @@ public class UtilController {
 	@Autowired
 	private CommonUtils utils;
 
-	@PostMapping("/uploadImage")
+	@GetMapping("/uploadImage")
 	public ResponseEntity<JwtResponse> uploadImageToCloudinary(@RequestParam("image") MultipartFile file)
 			throws IOException {
+		
+		System.out.println("Inside Upload Image ");
 
 		String res = utils.convertImageToUrl(file);
 
